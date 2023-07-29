fcd5325f4b6d8170b745352531fe2a7ab23fb4e0
TracyAntonia <aantonjuguna@gmail.com>
2023-07-29 04:06:09 +0300

    completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const[questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/questions')
+    .then(res => res.json())
+    .then(questions => setQuestions(questions));
+  },[])
+
+  function handleSubmitQuestion(postedQuestion) {
+    setQuestions([...questions,postedQuestion]);
+  }
 
   return (
     <main>
